00ad7c00d2ae9d9d9ecf7fb93b4a03035f65fd10
WoolDoughnut310 <wooldoughnutspi@outlook.com>
2022-05-18 06:47:09 +0100

    Added superscript formatting for ions

--- src/components/EquationSide.tsx
+++ src/components/EquationSide.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import styles from "./EquationSide.module.scss";
 import AddIcon from "@mui/icons-material/Add";
 import Stack from "@mui/material/Stack";
@@ -6,20 +7,28 @@ import Skeleton from "@mui/material/Skeleton";
 
 type Props = {
     compounds: string[];
+    ions: string[];
     balancingNumbers: number[];
 };
 
-export default function EquationSide({ compounds, balancingNumbers }: Props) {
-    const formatCompound = (compound: string, index: number) => {
+export default function EquationSide({
+    compounds,
+    ions,
+    balancingNumbers,
+}: Props) {
+    const [entities, setEntities] = useState<string[]>([]);
+
+    const formatEntity = (entity: string, index: number) => {
         let output = "";
         let inSubscript = false;
+        let inSuperscript = false;
 
         if (balancingNumbers[index] && balancingNumbers[index] > 1) {
             output = output.concat(balancingNumbers[index].toString());
         }
 
-        for (let i = 0; i < compound.length; i++) {
-            const char = compound[i];
+        for (let i = 0; i < entity.length; i++) {
+            const char = entity[i];
             const charCode = char.charCodeAt(0);
             const isNumber = !isNaN(parseInt(char));
 
@@ -35,6 +44,11 @@ export default function EquationSide({ compounds, balancingNumbers }: Props) {
                     inSubscript = false;
                 }
 
+                if (inSuperscript) {
+                    output = output.concat("</sup>");
+                    inSuperscript = false;
+                }
+
                 output = output.concat(char);
             } else if (!inSubscript && char === "_") {
                 // Start the subscript tag for later number(s)
@@ -42,16 +56,44 @@ export default function EquationSide({ compounds, balancingNumbers }: Props) {
                 output = output.concat("<sub>");
             } else if (inSubscript && isNumber) {
                 output = output.concat(char);
+            } else if (!inSuperscript && char === "[") {
+                // If previous character(s) were in subscript, end subscript tag
+                if (inSubscript) {
+                    output = output.concat("</sub>");
+                    inSubscript = false;
+                }
+
+                // Start the superscript tag for later ion charges
+                console.log("hi");
+                inSuperscript = true;
+                output = output.concat("<sup>");
+            } else if (inSuperscript && isNumber) {
+                output = output.concat(char);
+            } else if (inSuperscript && [43, 45].includes(charCode)) {
+                // Got the charge on the ion
+                output = output.concat(char);
             }
 
             // End subscript tag at last character, if numeric
-            if (i === compound.length - 1 && isNumber) {
+            if (i === entity.length - 1 && isNumber) {
                 output = output.concat("</sub>");
             }
         }
 
         return { __html: output };
     };
+
+    useEffect(() => {
+        let entities = [...compounds];
+        let lastIndex = Infinity;
+
+        ions.forEach((ion, i) => {
+            const insertIndex = i <= lastIndex ? i + 1 : i;
+            entities.splice(insertIndex, 0, ion);
+        });
+
+        setEntities(entities);
+    }, [compounds, ions]);
 
     return (
         <Stack
@@ -60,14 +102,11 @@ export default function EquationSide({ compounds, balancingNumbers }: Props) {
             className={styles.stack}
             divider={<AddIcon fontSize="large" />}
         >
-            {compounds.length === 0 && <Skeleton className={styles.skeleton} />}
-            {compounds.map((compound, index) => (
+            {entities.length === 0 && <Skeleton className={styles.skeleton} />}
+            {entities.map((entity, index) => (
                 <Typography key={index} variant="h4" fontWeight="bold">
                     <span
-                        dangerouslySetInnerHTML={formatCompound(
-                            compound,
-                            index
-                        )}
+                        dangerouslySetInnerHTML={formatEntity(entity, index)}
                     />
                 </Typography>
             ))}
